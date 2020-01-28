ff067b67d7805c14e5e9c865cb183c12947d63f3
Margiett Gil <mxg627@icloud.com>
2020-01-28 16:08:22 -0500

    Initial Commit

---
+++ Programmatic UI Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmatic UI Lab
+//
+//  Created by Margiett Gil on 1/28/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
